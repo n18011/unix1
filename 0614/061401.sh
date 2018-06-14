2a6da7a6302b675fe7255bc4473c74e3358d2e29
masahiro kina <n18011@std.it-college.ac.jp>
2018-06-14 07:46:02 +0000

    6/14の課題

---
+++ 0614/061401.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+year=$1
+mod1=$(($year%4))
+mod2=$(($year%100))
+mod3=$(($year%400))
+
+if [ $mod1 -eq 0 ] && [ ! $mod2 -eq 0 ] || [ $mod3 -eq 0 ]; then
+	echo $year is a leap year
+else
+	echo $year is not leap year
+fi
