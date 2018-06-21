7519db0fd7f07dcbc668c851923abf004eefdcd0
masahiro kina <n18011@std.it-college.ac.jp>
2018-06-21 02:19:22 +0000

    add txt

--- 0621/062101.sh
+++ 0621/062101.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+echo "1990年元旦からの曜日集計"
 for day in $(cat gantan)
 do
 	echo $(date -d $day '+%u %a')
-done | sort -n | uniq -c | awk '{print $3,$1}'
+done | sort -n | uniq -c | awk '{print $3,$1"回"}' 
