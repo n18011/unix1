16e48af183375b655c443d1fa8e2ec6dfece01c3
masahiro kina <n18011@std.it-college.ac.jp>
2018-06-28 03:15:50 +0000

    6/28テスト修正

--- 0628/062804.sh
+++ 0628/062804.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat personal_infomation.csv | tr ',' ' ' | awk '{print $7}' | sed 1d | sort -n | uniq -c | sort -nr | head -n 5
+cat personal_infomation.csv | tr ',' ' ' | awk '{print $7}' | sed 1d | sort | uniq -c | sort -nr | head -n 5 | awk '{print $2 $1"件"}'
