b756d28016d439e85440f59864a9ce59b201c516
masahiro kina <n18011@std.it-college.ac.jp>
2018-06-28 02:20:02 +0000

    6/28テスト

--- 0628/062802.sh
+++ 0628/062802.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-if [ $# -gt 0 ]; then
+if [ $# -eq 0 ]; then
 
-	find $1 -type f | xargs du -b | sort -nr | head -n 5
-else
 	echo 'Usage: 062802.sh PATH'
-
+	exit 1
 fi
+	find $1 -type f | xargs du -b | sort -nr | head -n 5
